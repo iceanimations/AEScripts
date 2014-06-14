dcbe34a17c1e568150e399fdf7bddc9876617260
Talha Ahmed <talha.ahmed@gmail.com>
2014-06-14 09:10:18 +0500

    Added gui to item settings

--- AdjustItemSettings.jsx
+++ AdjustItemSettings.jsx
@@ -9,28 +9,66 @@ var settings = {
     frameRate:25.0
 };
 
-var itemTypes = [ "Composition", "Footage" ];
+var itemTypes = [ 'Composition', 'Footage' ];
+
+var gui_settings = {
+    pixelAspect: {
+        enabled: true,
+        name: 'Pixel Aspect Ratio',
+        type: 'dropdownlist',
+        vals : {
+            0.91: 0.90909090909091,
+            1: 1,
+            1.5: 1.5, 
+            1.09: 1.09401709401709,
+            1.21: 1.21212121212121, 
+            1.33: 1.33333333333333, 
+            1.46: 1.45868945868946, 
+            2:2
+        },
+        dv: 2
+    },
+    frameRate: {
+        enabled: true,
+        name: 'FPS',
+        type: 'dropdownlist',
+        vals: {
+            24: 24.0,
+            25: 25.0,
+            30: 30.0,
+            50: 50.0,
+            60: 60.0,
+            120: 120.0,
+            180: 180.0,
+            300: 300.0
+        },
+        dv: 2,
+    },
+};
+
+g_debug = false;
 
 function exceptionalSetting(item, x, val, type) {
     switch (type) {
-        case "Footage":
-            source = item.useProxy ? "proxySource" : "mainSource";
+        case 'Footage':
+            source = item.useProxy ? 'proxySource' : 'mainSource';
             switch (x) {
-                case "frameRate":
+                case 'frameRate':
                     if (!item.isStill && !item[source].isStill) {
                         item[source].conformFrameRate = val;
-                        $.writeln("\"" + item.name + "\"." + x + " = " + val);
+                        if (g_debug)
+                            $.writeln("\"" + item.name + "\"." + x + " = " + val);
                     }
                     return true;
-                case "time":
+                case 'time':
                     if (item.isStill) return true;
                     break;
-                case "width":
-                case "height":
-                    if ("file" in item[source]) // filesource
+                case 'width':
+                case 'height':
+                    if ('file' in item[source]) // filesource
                         return true;
                     break;
-                case "duration":
+                case 'duration':
                     return true;
             } // switch x
     } // switch type
@@ -48,11 +86,12 @@ function applySettingsToItems(items, settings, itemTypes) {
                     try {
                         if (!exceptionalSetting(item, x, settings[x], itemType) && x in item) {
                             item[x]=settings[x];
-                            $.writeln("\"" + item.name + "\"." + x + " = " + settings[x]);
+                            if (g_debug)
+                                $.writeln('\"' + item.name + '\".' + x + ' = ' + settings[x]);
                         }
                     }
                     catch (err) {
-                        $.writeln([err, item.name + "." + x]);
+                        $.writeln([err, item.name + '.' + x]);
                     } // try ... catch
                 } // for
                 break; // from itemTypes
@@ -62,5 +101,87 @@ function applySettingsToItems(items, settings, itemTypes) {
     return true;
 }
 
-var items = app.project.items;
-applySettingsToItems(items, settings, itemTypes);
+function settingGroupsUI(panel) {
+    panel.grps = []
+    for ( s in gui_settings ) {
+        grp = panel.add('group');
+        grp.check = grp.add('checkbox', undefined, gui_settings[s].name);
+        grp.check.helpTip = s;
+        grp.check.value = gui_settings[s].enabled;
+        grp.s = s;
+        grp.gs = gui_settings[s];
+        panel.grps.push(grp)
+
+        if (grp.gs.type == 'dropdownlist') {
+            grp.ddl = grp.add('dropdownlist', undefined, 'value');
+            for (val in grp.gs.vals) {
+                grp.ddl.add('item', val)
+            }
+            grp.ddl.selection = grp.gs.dv
+            grp.check.onClick = function() {
+                this.parent.children[1].visible=this.value
+            }
+        }
+
+        else if (grp.gs.type == 'slider') {
+            grp.stsldr = grp.add('statictext', undefined, grp.gs.dv)
+            grp.sldr = grp.add('slider', undefined,
+                    grp.gs.dv,
+                    grp.gs.min,
+                    grp.gs.max)
+            grp.sldr.onChanging = function() {
+                this.parent.stsldr.text=this.value;
+            }
+            grp.check.onClick = function() {
+                this.parent.sldr.visible=this.value;
+                this.parent.stsldr.visible=this.value;
+            }
+        }
+    }
+}
+
+
+function doApply() {
+        dlg = this.parent.parent;
+        applyTo = [];
+        if (dlg.compCheck.value) applyTo.push('Composition');
+        if (dlg.footageCheck.value) applyTo.push('Footage');
+        settings = {};
+        for (i in dlg.settingsPanel.grps) {
+            grp = dlg.settingsPanel.grps[i];
+            key = grp.s;
+            val = 0;
+            if (grp.gs.type == 'slider')
+                val = grp.sldr.value;
+            else if (grp.gs.type == 'dropdownlist')
+                val = grp.gs.vals[grp.ddl.selection.text];
+            settings[key]=val;
+        }
+        applySettingsToItems(app.project.items, settings, applyTo);
+        dlg.close(true);
+}
+
+function applySettingsUI() {
+    dlg = new Window('dialog', 'Adjust Settings for all Items', undefined, {resizeable: true});
+
+    itemTypePanel = dlg.add('panel', undefined, 'Apply On:');
+    itemTypePanel.alignChildren = 'left';
+    dlg.compCheck = itemTypePanel.add('checkbox', undefined, 'Compositions');
+    dlg.compCheck.value = true;
+    dlg.footageCheck = itemTypePanel.add('checkbox', undefined, 'Footages');
+    dlg.footageCheck.value = true;
+
+    dlg.settingsPanel = dlg.add('panel', undefined, 'Settings');
+    dlg.settingsPanel.alignChildren = 'left';
+    settingGroupsUI(dlg.settingsPanel);
+
+    btnGrp = dlg.add('group');
+    applyBtn = btnGrp.add('button', undefined, 'Apply');
+    cancelBtn = btnGrp.add('button', undefined, 'Cancel');
+    applyBtn.onClick = doApply
+
+    dlg.center();
+    return dlg.show();
+}
+applySettingsUI()
+
